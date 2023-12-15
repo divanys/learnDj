15e86702eb585e82981547576ecccdc792f1904b
divan <hlinakcurbag@gmail.com>
2023-12-15 17:21:41 +0300

    we will add an images

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,7 @@ class Prosuct(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.CharField(max_length=300)
+    image = models.ImageField(blank=True, upload_to='images')
 
     def __str__(self):
         return self.name
