cecb2d1bc16fbe040e69b652c61dcd540fc94f5f
divan <hlinakcurbag@gmail.com>
2023-12-18 13:18:19 +0300

    балдеть не пердеть: update, delete, other urls and views

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = "myapp"
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('<int:item_id>/', views.indexItem, name='detailItem'),
     path('additem/', views.addItem, name='addItem'),
+    path('updateitem/<int:item_id>/', views.updateItem, name='updateItem'),
+    path('deleteitem/<int:item_id>/', views.deleteItem, name='deleteItem'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Prosuct
 
 
@@ -27,3 +27,32 @@ def addItem(request):
         item = Prosuct(name=name, price=price, description=description, image=image)
         item.save()
     return render(request, 'myapp/additem.html')
+
+
+def updateItem(request, item_id):
+    item = Prosuct.objects.get(id=item_id)
+
+    if request.method == "POST":
+        item.name = request.POST.get("name")
+        item.price = request.POST.get("price")
+        item.description = request.POST.get("description")
+        item.image = request.FILES.get('upload', item.image)
+        item.save()
+        return redirect('/myapp/')
+    context = {
+        'item': item
+    }
+    return render(request, 'myapp/updateitem.html', context)
+
+
+def deleteItem(request, item_id):
+    item = Prosuct.objects.get(id=item_id)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect('/myapp/')
+    context = {
+        'item': item
+    }
+    return render(request, 'myapp/deleteitem.html', context)
+
