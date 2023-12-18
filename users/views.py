396700561319cbe741de6e4c27bd5aef31869833
divan <hlinakcurbag@gmail.com>
2023-12-18 16:11:23 +0300

    балдеть не пердеть: сделала регистрацию пользователей (стд библ джанго), но я сделала! проблема паролей: долго тупила и не понимала, почему не пропускает регистрацию псвд 1234, вазхвах

---
+++ users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import NewUserForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('myapp:index')
+        else:
+            # Handle form errors, if any
+            print(form.errors)
+    form = NewUserForm()
+    context = {
+        'form': form
+    }
+
+    return render(request, 'users/register.html', context)
